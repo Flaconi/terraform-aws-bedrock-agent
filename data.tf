2eb050aa0c3ecfd07662ca52eb5f121d33013e23
Ronny Panknin <ronny.panknin@flaconi.de>
2025-10-08 11:36:18 +0200

    OPS-6934 separate bedrock:Rerank into dedicated statement with Resource="*"

--- data.tf
+++ data.tf
@@ -77,12 +77,18 @@ data "aws_iam_policy_document" "knowledgebase_trust" {
 data "aws_iam_policy_document" "knowledgebase_permissions" {
   statement {
     actions = [
-      "bedrock:InvokeModel",
-      "bedrock:Rerank"
+      "bedrock:InvokeModel"
     ]
     resources = [for id in local.knowledgebase_access_model_ids :
       data.aws_bedrock_foundation_model.knowledgebase[id].model_arn
     ]
+  }
+  statement {
+    sid     = "AllowRerank"
+    actions = [
+      "bedrock:Rerank"
+    ]
+    resources = ["*"]
   }
   statement {
     actions = ["aoss:APIAccessAll"]
